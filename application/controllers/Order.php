ea6035ba5de154a4caf5919a483dd3525bc250e1
Rezky Revindo <rezkyrevindo3@gmail.com>
2019-11-23 16:33:54 +0700

    add db

---
+++ application/controllers/Order.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Order extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Order.php */
+/* Location: ./application/controllers/Order.php */
