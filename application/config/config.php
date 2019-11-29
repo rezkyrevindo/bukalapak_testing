6417ebf56f27061212fc4117b08b2bc79a3ff5b3
unknown <Ainun Azizah>
2019-11-29 21:24:33 +0700

    Tampilan Login dan Register

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/bukalapak/';
+$config['base_url'] = 'http://localhost/bukalapak_testing/';
 
 /*
 |--------------------------------------------------------------------------
